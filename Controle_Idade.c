bbadc475673c21a7057c4774902cc4fb86944d1a
Kaio Bitencourt <kaiocesar1993@hotmail.com>
2021-05-25 21:47:49 -0300

    Atualizacao 1.3

--- Controle_Idade.c
+++ Controle_Idade.c
@@ -37,30 +37,26 @@ int CONFS(int* soma ,int* ano) {
                 }      
     }
 
-/*double PROD (double  pre[10]) {
 
-    (pre[0] = 10);
-}*/
-
-int PRODUTOS (int* cod, int* quant,double* preco,double total[10]) {
+int PRODUTOS (int* cod, int* quant,double preco[10],double total[10]) {
 
         printf("\nDEPARTAMENTO - BEBIDA ALCOOLICA: \n\n");
 
-        printf(" PRODUTO 1: Skol 473ml Preco por unidade: %.2lf\n",((*preco=4.5)));
-        printf(" PRODUTO 2: Original 350ml Preco por unidade: %.2lf\n",((*preco=4)));
-        printf(" PRODUTO 3: Brahma 350ml Preco por unidade: %.2lf\n",((*preco=3)));
-        printf(" PRODUTO 4: Amstel 473ml Preco por unidade: %.2lf\n",((*preco=5.5)));
-        printf(" PRODUTO 5: BudWeiser 350ml Preco por unidade: %.2lf\n",((*preco=4)));
-        printf(" PRODUTO 6: Bohemia 473ml Preco por unidade: %.2lf\n",((*preco=4.5)));
-        printf(" PRODUTO 7: Itaipava 473ml Preco por unidade: %.2lf\n",((*preco=3)));
-        printf(" PRODUTO 8: Schin 350ml Prec o por unidade: %.2lf\n\n",((*preco=2.5)));
+        printf(" PRODUTO 1: Skol 473ml Preco por unidade: %.2lf\n",((preco[0]=4.5)));
+        printf(" PRODUTO 2: Original 350ml Preco por unidade: %.2lf\n",((preco[1]=4)));
+        printf(" PRODUTO 3: Brahma 350ml Preco por unidade: %.2lf\n",((preco[2]=3)));
+        printf(" PRODUTO 4: Amstel 473ml Preco por unidade: %.2lf\n",((preco[3]=5.5)));
+        printf(" PRODUTO 5: BudWeiser 350ml Preco por unidade: %.2lf\n",((preco[4]=4)));
+        printf(" PRODUTO 6: Bohemia 473ml Preco por unidade: %.2lf\n",((preco[5]=4.5)));
+        printf(" PRODUTO 7: Itaipava 473ml Preco por unidade: %.2lf\n",((preco[6]=3)));
+        printf(" PRODUTO 8: Schin 350ml Prec o por unidade: %.2lf\n\n",((preco[7]=2.5)));
 
         printf("DESCRICAO DA COMPRA\n");
 
-        while ((*cod) < 1 || (*cod) > 8){
+        do {
             printf("\n Digite o codigo do produto: ");
             scanf("%d", cod);
-        }
+        } while((*cod) < 1 || (*cod) > 7);
        
         printf(" Digite a quantidade: ");
         scanf("%d", quant);
@@ -68,63 +64,66 @@ int PRODUTOS (int* cod, int* quant,double* preco,double total[10]) {
             switch((*cod)) {
 
         case 1:
-        printf("\n CONFIRMANDO: %d unidades de Skol 473ml\n",(*quant));
-        (*preco) = 4.5;
-        (total[0]) = ((*preco) * (*quant));
+        printf("\n CONFIRMANDO: %d unidades de Skol 473ml\n\n",(*quant));
+        preco[0] = 4.5;
+        total[0] = preco[0] * (*quant);
         break;
 
         case 2:
-        printf("\n CONFIRMANDO: %d unidades de Original 350ml\n",(*quant));
-        (*preco) = 4;
-        (total[1]) = ((*preco) * (*quant));
+        printf("\n CONFIRMANDO: %d unidades de Original 350ml\n\n",(*quant));
+        preco[1] = 4;
+        total[1] = preco[1] * (*quant);
         break;
 
         case 3:
-        printf("\n CONFIRMANDO: %d unidades de Brahma 350ml\n",(*quant));
-        (*preco) = 3;
-        (*total) = (*preco) * (*quant);
+        printf("\n CONFIRMANDO: %d unidades de Brahma 350ml\n\n",(*quant));
+        preco[2] = 3;
+        total[2] = preco[2] * (*quant);
         break;
 
         case 4:
-        printf("\n CONFIRMANDO: %d unidades de Amstel 473ml\n",(*quant));
-        (*preco) = 5.5;
-        (*total) = (*preco) * (*quant);
+        printf("\n CONFIRMANDO: %d unidades de Amstel 473ml\n\n",(*quant));
+        preco[3] = 5.5;
+        total[3] = preco[3] * (*quant);
         break;
 
         case 5:
-        printf("\n CONFIRMANDO: %d unidades de BudWeiser 350ml\n",(*quant));
-        (*total) = (*preco) * (*quant);
-        (*preco) = 4;
+        printf("\n CONFIRMANDO: %d unidades de BudWeiser 350ml\n\n",(*quant));
+        preco[4] = 4;
+        total[4] = preco[4] * (*quant);
         break;
 
         case 6:
-        printf("\n CONFIRMANDO: %d unidades de Bohemia 473ml\n",(*quant));
-        (*preco) = 4.5;
-        (*total) = (*preco) * (*quant);
+        printf("\n CONFIRMANDO: %d unidades de Bohemia 473ml\n\n",(*quant));
+        preco[5] = 4.5;
+        total[5] = preco[5] * (*quant);
         break;
 
         case 7:
-        printf("\n CONFIRMANDO: %d unidades de Itaipava 473ml\n",(*quant));
-        (*preco) = 3;
-        (*total) = (*preco) * (*quant);
+        printf("\n CONFIRMANDO: %d unidades de Itaipava 473ml\n\n",(*quant));
+        preco[6] = 3;
+        total[6] = preco[6] * (*quant);
         break;
 
         case 8:
         printf("\n CONFIRMANDO: %d unidades de Schin 350ml\n",(*quant));
-        (*preco) = 2.5;
-        (*total) = (*preco) * (*quant);
+        preco[7] = 2.5;
+        total[7] = preco[7] * (*quant);
         break;
     }
 }
 
 double COMPRAS(double total[10],double valor[10]) {
 
+    valor[0] = total[0];
+    valor[1] = total[2];
+    valor[2] = total[1];
+    valor[3] = total[3];
+    valor[4] = total[4];
+    valor[5] = total[5];
+    valor[6] = total[6];
 
-    (valor[10]) = total[10] + total[10];
-    printf(" Total: %.2lf Reais\n",(*valor));  
-    
-   
-     
+    printf(" Total: %.2lf Reais\n",((valor[0])+(valor[1])+(valor[2])+(valor[3])+(valor[4])+(valor[5])+(valor[6])));
 }
 
 int main() {
@@ -133,9 +132,9 @@ int main() {
     int dia,mes,ano;
     char letra;
     int quant,cod;
-    double total[10],valor[10];
-    double preco;
-
+    double total[10];
+    double valor[10];
+    double preco[10];
 
     do {
         DD(&dia); MES(&mes); ANO(&ano);
@@ -149,73 +148,16 @@ int main() {
         }
 
     do {
-        PRODUTOS(&cod, &quant, &preco, &total[10]);
+        PRODUTOS(&cod, &quant, &preco[10], &total[10]);
 
         printf(" Deseja finalizar a compra S/N: ");
 
         if(getchar()== 'N');
+
     } while(getchar()=='N');
 
     COMPRAS(&total[10], &valor[10]);
    
     return 0;
 }
-
-
-/*switch((*cod)) {
-
-        case 1:
-        printf("\n CONFIRMANDO: %d unidades de Skol 473ml\n",(*quant));
-        (*preco) = 4.5;
-        (*total) = (*preco) * (*quant);
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;
-
-        case 2:
-        printf("\n CONFIRMANDO: %d unidades de Original 350ml\n",(*quant));
-        (*preco) = 4;
-        (*total) = (*preco) * (*quant);
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;
-
-        case 3:
-        printf("\n CONFIRMANDO: %d unidades de Brahma 350ml\n",(*quant));
-        (*preco) = 3;
-        (*total) = (*preco) * (*quant);
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;
-
-        case 4:
-        printf("\n CONFIRMANDO: %d unidades de Amstel 473ml\n",(*quant));
-        (*preco) = 5.5;
-        (*total) = (*preco) * (*quant);
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;
-
-        case 5:
-        printf("\n CONFIRMANDO: %d unidades de BudWeiser 350ml\n",(*quant));
-        (*total) = (*preco) * (*quant);
-        (*preco) = 4;
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;
-
-        case 6:
-        printf("\n CONFIRMANDO: %d unidades de Bohemia 473ml\n",(*quant));
-        (*preco) = 4.5;
-        (*total) = (*preco) * (*quant);
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;
-
-        case 7:
-        printf("\n CONFIRMANDO: %d unidades de Itaipava 473ml\n",(*quant));
-        (*preco) = 3;
-        (*total) = (*preco) * (*quant);
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;
-
-        case 8:
-        printf("\n CONFIRMANDO: %d unidades de Schin 350ml\n",(*quant));
-        (*preco) = 2.5;
-        (*total) = (*preco) * (*quant);
-        printf(" Total: %.2lf Reais\n",(*total));
-        break;*/
+    
