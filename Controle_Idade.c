da277b7d486b6ceb4bd1f3860775413e6f9b5baf
Kaio Bitencourt <kaiocesar1993@hotmail.com>
2021-07-22 20:49:06 -0300

    Atualizacao 2.4

--- Controle_Idade.c
+++ Controle_Idade.c
@@ -88,7 +88,7 @@ int DEPARTAMENTO (int* dep, char* codep) {
         } while ((*dep) < 1 || (*dep) > 3);        
 }
 
-int CERVEJA (int* cod, int* quant, int* dep, int contador[30], double preco[30],double total[30],int i) {
+int CERVEJA (int* cod, int* quant, int* dep, double preco[30], double total[30],int i) {
     switch(*cod) {
        case 1:
         printf("\n CONFIRMANDO: %d unidades de Skol 473ml\n",(*quant));
@@ -220,7 +220,7 @@ int CERVEJA (int* cod, int* quant, int* dep, int contador[30], double preco[30],
     (*dep) = 1;
 }
 
-int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30], double wtotal[30], int j) {
+int WHISKY (int* cod, int* quant, int* dep, double wpreco[30], double wtotal[30], int j) {
     switch(*cod) {
         case 1:
         printf("\n CONFIRMANDO %d unidades de Red Label 750ml\n",(*quant));
@@ -267,7 +267,7 @@ int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30],
 
         for(j = 0; j <=contador3; j++)
             wtotal[3] = wtotal[3] + wtotal3[j];
-            contador++;
+            contador3++;
             break;
 
         case 4:
@@ -277,7 +277,7 @@ int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30],
         int contador4 = 0;
 
         for(j = 0; j <= contador4; j++) {
-            wtotal4[j] = wpreco[4] + (*quant);
+            wtotal4[j] = wpreco[4] * (*quant);
             printf(" TOTAL: %.2lf\n\n",wtotal4[j]);
         }
 
@@ -293,7 +293,7 @@ int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30],
         int contador5 = 0;
 
         for (j = 0; j<=contador5; j++) {
-            wtotal5[j] = wpreco[5] + (*quant);
+            wtotal5[j] = wpreco[5] * (*quant);
             printf(" TOTAL: %.2lf\n\n",wtotal5[j]);
         }
 
@@ -309,7 +309,7 @@ int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30],
         int contador6 = 0;
 
         for (j = 0; j<=contador6; j++) {
-            wtotal6[j] = wpreco[6] + (*quant);
+            wtotal6[j] = wpreco[6] * (*quant);
             printf(" TOTAL: %.2lf\n\n",wtotal6[j]);
         }
 
@@ -325,7 +325,7 @@ int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30],
         int contador7 = 0;
 
         for (j = 0; j<=contador7; j++) {
-            wtotal7[j] = wpreco[7] + (*quant);
+            wtotal7[j] = wpreco[7] * (*quant);
             printf(" TOTAL: %.2lf\n\n",wtotal7[j]);
         }
 
@@ -341,7 +341,7 @@ int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30],
         int contador8 = 0;
 
         for (j = 0; j<=contador8; j++) {
-            wtotal8[j] = wpreco[8] + (*quant);
+            wtotal8[j] = wpreco[8] * (*quant);
             printf(" TOTAL: %.2lf\n\n",wtotal8[j]);
         }
 
@@ -351,6 +351,91 @@ int WHISKY (int* cod, int* quant, int* dep, int contador[30], double wpreco[30],
             break;
         }
         (*dep) = 2;
+}
+
+int GIN(int* cod, int* quant, int* dep, double gpreco[30], double gtotal[30], int k) {
+    switch(*cod){
+        case 1:
+        printf("\n CONFIRMANDO %d unidades de Tanqueray 750ml\n",(*quant));
+        gpreco[1] = 105;
+        double gtotal1[10];
+        int contador1 = 0;
+
+        for(k = 0; k <=contador1; k++) {
+            gtotal1[k] = gpreco[1] * (*quant);
+            printf(" TOTAL: %.2lf Reais\n\n",gtotal1[k]);
+        }
+
+        for(k = 0; k <=contador1; k++)
+            gtotal[1] = gtotal[1] + gtotal1[k];
+            contador1++;
+            break;
+
+        case 2:
+        printf("\n CONFIRMANDO %d unidades de Tanqueray Sevilla 750ml\n",(*quant));
+        gpreco[2] = 110;
+        double gtotal2[10];
+        int contador2 = 0;
+
+        for(k = 0; k <=contador2; k++) {
+            gtotal2[k] = gpreco[2] * (*quant);
+            printf(" Total: %.2lf Reais\n\n",gtotal2[k]);
+        }
+
+        for(k = 0; k <=contador2; k++)
+            gtotal[2] = gtotal[2] + gtotal2[k];
+            contador2++;
+            break;
+
+        case 3:
+        printf("\n CONFIRMANDO %d unidades de Beefeater Dry 750ml\n",(*quant));
+        gpreco[3] = 70;
+        double gtotal3[10];
+        int contador3 = 0;
+
+        for(k = 0; k <=contador3; k++) {
+            gtotal3[k] = gpreco[3] * (*quant);
+            printf(" TOTAL: %.2lf\n\n",gtotal3[k]);
+        }
+
+        for(k = 0; k <=contador3; k++)
+            gtotal[3] = gtotal[3] + gtotal3[k];
+            contador3++;
+            break;
+
+        case 4:
+        printf("\n CONFIRMANDO %d unidades de Gordon'S Classico 750ml\n",(*quant));
+        gpreco[4] = 67;
+        double gtotal4[10];
+        int contador4 = 0;
+
+        for(k = 0; k <=contador4; k++) {
+            gtotal4[k] = gpreco[4] * (*quant);
+            printf(" TOTAL: %.2lf\n\n",gtotal4[k]);
+        }
+
+        for(k = 0; k <=contador4; k++)
+            gtotal[4] = gtotal[4] + gtotal4[k];
+            contador4++;
+            break;
+
+        case 5:
+        printf("\n CONFIRMANDO %d unidades de Gordon'S Pink 750ml\n",(*quant));
+        gpreco[5] = 67;
+        double gtotal5[10];
+        int contador5 = 0;
+
+        for(k = 0; k <=contador5; k++) {
+            gtotal5[k] = gpreco[5] * (*quant);
+            printf(" TOTAL: %.2lf\n\n",gtotal5[k]);
+        }
+
+        for(k = 0; k <=contador5; k++)
+            gtotal[5] = gtotal[5] + gtotal5[k];
+            contador5++;
+            break;
+        }
+        (*dep) = 3;
 }
 
 double COMPCER(double total[30], double somatotal,int i) {
@@ -383,18 +468,36 @@ somawis = 0;
         for(j = 1; j <=8; j++)
             somawis = somawis + wtotal[j];
         printf("\n Soma total: %.2lf Reais\n",somawis);
+}
 
+int COMGIN(double gtotal[30], double somagin, int k) {
+    somagin = 0;
+   
+        printf("\n DEPARTAMENTO - BEBIDA ALCOOLICA: (GIN) \n\n");
+
+        for(k = 1; k <=8; k++) {
+            if(gtotal[k] > 1) {
+                printf(" PRODUTO %d: %.2lf Reais\n",k,gtotal[k]);
+                }
+        }
+    
+        for(k = 1; k <=8; k++)
+            somagin = somagin + gtotal[k];
+        printf("\n Soma total: %.2lf Reais\n",somagin);
 }
 
 int main() {
 
-    int dia,mes,ano,cod,somaidade,i,j,quant,dep,contador[30];
+    int dia,mes,ano,cod,somaidade,i,j,k,quant,dep;
     char data,maio,adata,quantid,codigo,codep;
-    double somawis,somatotal;
+    double somawis,somatotal,somagin;
+
     double total[30] = {0};
     double preco[30] = {0};
     double wpreco[30] = {0};
     double wtotal[30] = {0};
+    double gtotal[30] = {0};
+    double gpreco[30] = {0};
     char resp2[2] = "n";
     char resp1[2] = "N";
     char resp[2] = " ";
@@ -451,7 +554,7 @@ int main() {
        
         QUANTIDADE(&quant,&quantid);
      
-        CERVEJA(&cod, &quant, &dep, &contador[30], &preco[30], &total[30],i);
+        CERVEJA(&cod, &quant, &dep, &preco[30], &total[30],i);
         break;
 
         case 2:
@@ -472,7 +575,25 @@ int main() {
        
         QUANTIDADE(&quant,&quantid);
 
-        WHISKY(&cod, &quant, &dep, &contador[30], &wpreco[30], &wtotal[30],j);
+        WHISKY(&cod, &quant, &dep, &wpreco[30], &wtotal[30],j);
+        break;
+
+        case 3:
+        printf("\n DEPARTAMENTO - BEBIDA ALCOOLICA: (GIN) \n\n");
+        printf(" PRODUTO 1: Tanqueray 750ml Preco por unidade: 105.00 Reais\n");
+        printf(" PRODUTO 2: Tanqueray Sevilla 750ml Preco por unidade: 110.00 Reais\n");
+        printf(" PRODUTO 3: Beefeater Dry 750ml Preco por unidade: 70.00 Reais\n");
+        printf(" PRODUTO 4: Gordon'S Classico 750ml Preco por unidade: 67.00 Reais\n");
+        printf(" PRODUTO 5: Gordon'S Pink 750ml Preco por unidade: 69.00 Reais\n\n");
+
+        printf(" DESCRICAO DA COMPRA:\n");
+        printf("\n");
+
+        CODIGO(&cod,&codigo);
+       
+        QUANTIDADE(&quant,&quantid);
+
+        GIN(&cod, &quant, &dep, &gpreco[30], &gtotal[30],k);
         break;
 }
     do {
@@ -498,10 +619,17 @@ int main() {
 
     } while(strcmp(res,resp));
 
-    if(dep == 1) {
+    switch(dep) {
+        case 1:
         COMPCER(&total[30], somatotal, i);
-   } else {
+        break;
+
+        case 2:
         COMPWIS(&wtotal[30], somawis, j);
+        break;
+
+        case 3:
+        COMGIN(&gtotal[30], somagin, k);
    }
 
     return 0;
